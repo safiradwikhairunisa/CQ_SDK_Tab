c1a7554967f65a7b78eb640a30c71a6e7710b978
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2026-03-31 18:43:49 +0700

    koneksi

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -19,5 +19,9 @@ namespace PraktikumADO
         {
             InitializeComponent();
         }
+        private void Koneksi()
+        {
+            conn = new SqlConnection("Data Source=PIREE\\FIRA;Initial Catalog=DBAkademikADO;Integrated Security=True");
+        }
     }
 }
