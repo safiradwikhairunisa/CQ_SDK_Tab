3a09e74644c6e5a3ef755a8fb45b7b6e97b2ea3f
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2026-03-31 18:54:01 +0700

    add catch button sks

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -155,5 +155,23 @@ namespace PraktikumADO
                 MessageBox.Show(ex.Message);
             }
         }
+
+        private void btnInsertProdi_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+
+                string query = "INSERT INTO ProgramStudi VALUES ('MI01','Manajemen Informatika')";
+
+                cmd = new SqlCommand(query, conn);
+
+                int hasil = cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Jumlah baris terpengaruh : " + hasil);
+
+                conn.Close();
+            }
     }
 }
