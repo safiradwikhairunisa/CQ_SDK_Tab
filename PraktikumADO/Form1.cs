097f7efbf9a0b28e939715d9b923a46cd35d624c
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2026-03-31 18:54:41 +0700

    add btnProdi

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -173,5 +173,10 @@ namespace PraktikumADO
 
                 conn.Close();
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
