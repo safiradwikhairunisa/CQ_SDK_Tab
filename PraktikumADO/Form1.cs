e57bdce802539e2c53013c633cbc8b2490a88b63
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2026-03-31 18:30:21 +0700

    koneksi

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace PraktikumADO
 {
     public partial class Form1 : Form
     {
+        SqlConnection conn;
+        SqlCommand cmd;
+
         public Form1()
         {
             InitializeComponent();
